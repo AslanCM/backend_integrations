d82dbc3b4947557be8bc95d71685fad13d1cbb88
AslanCM <aslandode@gmail.com>
2025-10-16 03:21:40 -0500

    no src

--- src/components/healthcheck/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-const { healthcheck } = require('./controller');
-
-router.get('/health', healthcheck);
-
-module.exports = router;
